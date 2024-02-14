596a882540551f94e4a8d7d495882bd5d864e67f
Vadim <sekret.vad@gmail.com>
2024-02-14 16:10:13 +0300

    add fixtures and parse

--- gendiff.js
+++ gendiff.js
@@ -2,13 +2,19 @@
 import { Command } from 'commander';
 const program = new Command();
 
+import parse from './functions/parserFunc.js'
+
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .helpOption('-h, --help','output usage information')
   .action((filepath1, filepath2) => {
-    console.log(parser(filepath1, filepath2, program.opts().format));
+    const oneObj = parse(filepath1)
+    const twoObj = parse(filepath2)
+    console.log(oneObj)
+    console.log(twoObj)
   });
 program.parse();
